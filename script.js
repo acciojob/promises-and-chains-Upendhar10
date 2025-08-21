3d29505d9e32c1ff7685c47d6507d67d4913f81b
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-08-21 20:17:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+let form = document.querySelector('#userForm');
+
+form.addEventListener("submit", ValidateForm);
+
+function ValidateForm(e){
+	e.preventDefault();
+
+	let userName = form.elements["name"].value.trim();
+	let userAge = form.elements["age"].value.trim();
+
+	if(!userName || !userAge){
+		alert("Please enter valid details");
+		return;
+	}
+
+	// create a promise
+	const submitForm = new Promise((resolve, reject) => {
+
+		if(userName === "John" && userAge > 18){
+			resolve(`Welcome, ${userName}. You can vote`);
+		
+		}else if (userName === 'Doe' && userAge < 18){
+			reject(`Oh Sorry, ${userName}. You aren't old enough`);
+	}
+
+	// Handle Promise Outcome
+	submitForm
+		.then((message) => {
+			setTimeout(() => {
+				alert(message);
+				form.submit();
+			}, 4000);
+		})
+		.catch(() => {
+			setTimeout(() => {
+				alert(message);
+				form.submit();
+			}, 4000);
+		})
+}
